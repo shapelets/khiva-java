0352b9ca7b7c5244e7f0e29b289ebca39b411fcb
Oscar Torreno <oscar.torreno@shapelets.io>
2018-06-05 14:56:51 +0000

    Merged in feature/operators (pull request #38)
    Feature/operators
    
    * Adding the array operators
    
    * Refactor to delete the unnecessary passing of the reference, since it is already contained in the object
    
    * Minor changes to documentation
    
    * Minor changes to documentation
    
    * Auto-formatting
    
    * Fixing documentation
    
    * Passed as parameter fix
    
    Approved-by: Antonio Vilches <antonio.vilches@shapelets.io>

--- src/main/java/com/gcatsoft/tsa/Array.java
+++ src/main/java/com/gcatsoft/tsa/Array.java
@@ -228,7 +228,10 @@ public class Array extends Library implements AutoCloseable {
 
     public Array(long ref) {
         this.reference = ref;
+    }
 
+    public Array(Array other) {
+        this.reference = other.copy().reference;
     }
 
     private native long createArrayFromDouble(double[] arr, long[] dims);
@@ -249,31 +252,84 @@ public class Array extends Library implements AutoCloseable {
 
     private native long createArrayFromDoubleComplex(DoubleComplex[] arr, long[] dims);
 
-    private native void deleteArray(long ref);
+    private native void deleteArray();
 
-    private native long[] getDims(long ref);
+    private native long[] nativeGetDims();
 
-    private native long print(long ref);
+    private native long nativePrint();
 
-    private native DoubleComplex[] getDoubleComplexFromArray(long ref);
+    private native DoubleComplex[] getDoubleComplexFromArray();
 
-    private native FloatComplex[] getFloatComplexFromArray(long ref);
+    private native FloatComplex[] getFloatComplexFromArray();
 
-    private native double[] getDoubleFromArray(long ref);
+    private native double[] getDoubleFromArray();
 
-    private native float[] getFloatFromArray(long ref);
+    private native float[] getFloatFromArray();
 
-    private native short[] getShortFromArray(long ref);
+    private native short[] getShortFromArray();
 
-    private native byte[] getByteFromArray(long ref);
+    private native byte[] getByteFromArray();
 
-    private native boolean[] getBooleanFromArray(long ref);
+    private native boolean[] getBooleanFromArray();
 
-    private native int[] getIntFromArray(long ref);
+    private native int[] getIntFromArray();
 
-    private native long[] getLongFromArray(long ref);
+    private native long[] getLongFromArray();
 
-    private native int getType(long ref);
+    private native int nativeGetType();
+
+    private native long[] add(long refRhs);
+
+    private native long[] mul(long refRhs);
+
+    private native long[] sub(long refRhs);
+
+    private native long[] div(long refRhs);
+
+    private native long[] mod(long refRhs);
+
+    private native long[] pow(long refRhs);
+
+    private native long[] lt(long refRhs);
+
+    private native long[] gt(long refRhs);
+
+    private native long[] le(long refRhs);
+
+    private native long[] ge(long refRhs);
+
+    private native long[] eq(long refRhs);
+
+    private native long[] ne(long refRhs);
+
+    private native long[] bitAnd(long refRhs);
+
+    private native long[] bitOr(long refRhs);
+
+    private native long[] bitXor(long refRhs);
+
+    private native long nativeBitShiftL(int n);
+
+    private native long nativeBitShiftR(int n);
+
+    private native long nativeNot();
+
+    private native long nativeTranspose(boolean conjugate);
+
+    private native long nativeCol(int index);
+
+    private native long nativeCols(int first, int last);
+
+    private native long nativeRow(int index);
+
+    private native long nativeRows(int first, int last);
+
+    private native long[] matmul(long refRhs);
+
+    private native long nativeCopy();
+
+    private native long as(int type);
+
 
     /**
      * Gets the Array reference.
@@ -299,7 +355,7 @@ public class Array extends Library implements AutoCloseable {
      * @return Array data type.
      */
     public Dtype getType() {
-        return Dtype.values()[getType(this.reference)];
+        return Dtype.values()[nativeGetType()];
     }
 
     /**
@@ -334,31 +390,31 @@ public class Array extends Library implements AutoCloseable {
     public <Any> Any getData() {
         switch (getType().ordinal()) {
             case 0:
-                return (Any) getFloatFromArray(this.reference);
+                return (Any) getFloatFromArray();
             case 1:
-                return (Any) getFloatComplexFromArray(this.reference);
+                return (Any) getFloatComplexFromArray();
             case 2:
-                return (Any) getDoubleFromArray(this.reference);
+                return (Any) getDoubleFromArray();
             case 3:
-                return (Any) getDoubleComplexFromArray(this.reference);
+                return (Any) getDoubleComplexFromArray();
             case 4:
-                return (Any) getBooleanFromArray(this.reference);
+                return (Any) getBooleanFromArray();
             case 5:
-                return (Any) getIntFromArray(this.reference);
+                return (Any) getIntFromArray();
             case 6:
-                return (Any) getIntFromArray(this.reference);
+                return (Any) getIntFromArray();
             case 7:
-                return (Any) getByteFromArray(this.reference);
+                return (Any) getByteFromArray();
             case 8:
-                return (Any) getLongFromArray(this.reference);
+                return (Any) getLongFromArray();
             case 9:
-                return (Any) getLongFromArray(this.reference);
+                return (Any) getLongFromArray();
             case 10:
-                return (Any) getShortFromArray(this.reference);
+                return (Any) getShortFromArray();
             case 11:
-                return (Any) getShortFromArray(this.reference);
+                return (Any) getShortFromArray();
             default:
-                return (Any) getFloatFromArray(this.reference);
+                return (Any) getFloatFromArray();
         }
     }
 
@@ -366,7 +422,7 @@ public class Array extends Library implements AutoCloseable {
      * Prints the Array.
      */
     public void print() {
-        this.reference = print(this.reference);
+        nativePrint();
     }
 
     /**
@@ -375,12 +431,324 @@ public class Array extends Library implements AutoCloseable {
      * @return The dimensions.
      */
     public long[] getDims() {
-        return getDims(this.reference);
+        return nativeGetDims();
     }
 
+    /**
+     * Adds this array with the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The sum of both arrays.
+     */
+    public Array add(Array rhs) {
+        long[] refs = add(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Multiplies this array with the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The product of both arrays.
+     */
+    public Array mul(Array rhs) {
+        long[] refs = mul(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Subtracts this array with the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The subtraction of both arrays.
+     */
+    public Array sub(Array rhs) {
+        long[] refs = sub(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Divides this array with the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The division of both arrays.
+     */
+    public Array div(Array rhs) {
+        long[] refs = div(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Performs the modulo operation of this array with the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The modulo of this array with the one passed as parameter.
+     */
+    public Array mod(Array rhs) {
+        long[] refs = mod(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Powers this array with the one specified as exponent parameter.
+     *
+     * @param exponent Exponent for the power operation.
+     * @return The power of this array with the one passed as parameter.
+     */
+    public Array pow(Array exponent) {
+        long[] refs = pow(exponent.reference);
+        exponent.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Compares (element-wise) if this array is lower than the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The result of comparing element-wise this array with the one passed as parameter.
+     */
+    public Array lt(Array rhs) {
+        long[] refs = lt(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Compares (element-wise) if this array is greater than the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The result of comparing element-wise this array with the one passed as parameter.
+     */
+    public Array gt(Array rhs) {
+        long[] refs = gt(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Compares (element-wise) if this array is lower or equal than the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The result of comparing element-wise this array with the one passed as parameter.
+     */
+    public Array le(Array rhs) {
+        long[] refs = le(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Compares (element-wise) if this array is greater or equal than the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The result of comparing element-wise this array with the one passed as parameter.
+     */
+    public Array ge(Array rhs) {
+        long[] refs = ge(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Compares (element-wise) if this array is equal to the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The result of comparing element-wise this array with the one passed as parameter.
+     */
+    public Array eq(Array rhs) {
+        long[] refs = eq(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Compares (element-wise) if this array is not equal to the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The result of comparing element-wise this array with the one passed as parameter.
+     */
+    public Array ne(Array rhs) {
+        long[] refs = ne(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Performs an AND operation with this array and the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The result of an AND operation of this array with the one passed as parameter.
+     */
+    public Array bitAnd(Array rhs) {
+        long[] refs = bitAnd(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Performs an OR operation with this array and the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The result of an OR operation of this array with the one passed as parameter.
+     */
+    public Array bitOr(Array rhs) {
+        long[] refs = bitOr(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Performs an eXclusive-OR operation with this array and the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The result of an eXclusive-OR operation of this array with the one passed as parameter.
+     */
+    public Array bitXor(Array rhs) {
+        long[] refs = bitXor(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Performs a left bit shift operation to this array (element-wise) as many times as specified in the parameter n.
+     *
+     * @param n Number of bits to be shifted.
+     * @return The result of a left bit shift operation to this array as many times as specified in the
+     * parameter.
+     */
+    public Array bitShiftL(int n) {
+        long ref = nativeBitShiftL(n);
+        return new Array(ref);
+    }
+
+    /**
+     * Performs a right bit shift operation to this array (element-wise) as many times as specified in the parameter n.
+     *
+     * @param n Number of bits to be shifted.
+     * @return The result of a right bit shift operation to this array as many times as specified in the
+     * parameter.
+     */
+    public Array bitShiftR(int n) {
+        long ref = nativeBitShiftR(n);
+        return new Array(ref);
+    }
+
+    /**
+     * Logical NOT operation to this array.
+     *
+     * @return The result of a logical NOT operation to this array.
+     */
+    public Array not() {
+        long ref = nativeNot();
+        return new Array(ref);
+    }
+
+    /**
+     * Transposes this array.
+     *
+     * @param conjugate If true a conjugate transposition is performed.
+     * @return The transposed (conjugate) array.
+     */
+    public Array transpose(boolean conjugate) {
+        long ref = nativeTranspose(conjugate);
+        return new Array(ref);
+    }
+
+    /**
+     * Transposes this array without conjugating it.
+     *
+     * @return The transposed array.
+     */
+    public Array transpose() {
+        return transpose(false);
+    }
+
+    /**
+     * Returns the specified column by index from this array.
+     *
+     * @param index The column to be retrieved.
+     * @return The specified column of this array.
+     */
+    public Array col(int index) {
+        long ref = nativeCol(index);
+        return new Array(ref);
+    }
+
+    /**
+     * Retrieves a subset of columns from this array, starting at first and finishing at last, both inclusive.
+     *
+     * @param first Start of the subset of columns to be retrieved.
+     * @param last  End of the subset of columns to be retrieved.
+     * @return The subset of columns of this array starting at first and finishing at last, both inclusive.
+     */
+    public Array cols(int first, int last) {
+        long ref = nativeCols(first, last);
+        return new Array(ref);
+    }
+
+    /**
+     * Retrieves a given row from this array.
+     *
+     * @param index The row to be retrieved.
+     * @return The specified row of this array.
+     */
+    public Array row(int index) {
+        long ref = nativeRow(index);
+        return new Array(ref);
+    }
+
+    /**
+     * Retrieves a subset of rows from this array, starting at first and finishing at last, both inclusive.
+     *
+     * @param first Start of the subset of rows to be retrieved.
+     * @param last  End of the subset of rows to be retrieved.
+     * @return The subset of rows of this array starting at first and finishing at last, both inclusive.
+     */
+    public Array rows(int first, int last) {
+        long ref = nativeRows(first, last);
+        return new Array(ref);
+    }
+
+    /**
+     * Performs a matrix multiplication of this array with the one passed as parameter.
+     *
+     * @param rhs Right-hand side array for the operation.
+     * @return The result of a matrix multiplication of both arrays.
+     */
+    public Array matmul(Array rhs) {
+        long[] refs = matmul(rhs.reference);
+        rhs.reference = refs[0];
+        return new Array(refs[1]);
+    }
+
+    /**
+     * Performs a deep copy of this array. Both the data stored in the device and all the object properties in Java.
+     *
+     * @return A deep copy of this array.
+     */
+    public Array copy() {
+        long ref = nativeCopy();
+        return new Array(ref);
+    }
+
+    /**
+     * Changes the type of this array.
+     *
+     * @param type Target type of the output array.
+     * @return The result of changing the type of the input array to the one passed as parameter.
+     */
+    public Array as(Dtype type) {
+        long ref = as(type.ordinal());
+        return new Array(ref);
+    }
 
     @Override
     public void close() throws Exception {
-        deleteArray(this.reference);
+        deleteArray();
     }
 }
--- src/test/java/com/gcatsoft/tsa/ArrayTest.java
+++ src/test/java/com/gcatsoft/tsa/ArrayTest.java
@@ -182,4 +182,347 @@ public class ArrayTest {
         Array a = new Array(tss, dims);
         Assert.assertEquals(a.getType(), Array.Dtype.s64);
     }
+
+    @Test
+    public void testAdd() throws Exception {
+        float[] data = {1f, 2f, 3f, 4f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = new Array(data, dims);
+        Array c = a.add(b);
+
+        float[] result = c.getData();
+        float[] expected = {2f, 4f, 6f, 8f};
+        Assert.assertArrayEquals(result, expected, 1e-6f);
+    }
+
+    @Test
+    public void testMul() throws Exception {
+        float[] data = {1f, 2f, 3f, 4f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = new Array(data, dims);
+        Array c = a.mul(b);
+
+        float[] result = c.getData();
+        float[] expected = {1f, 4f, 9f, 16f};
+        Assert.assertArrayEquals(result, expected, 1e-6f);
+    }
+
+    @Test
+    public void testSub() throws Exception {
+        float[] data = {1f, 2f, 3f, 4f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = new Array(data, dims);
+        Array c = a.sub(b);
+
+        float[] result = c.getData();
+        float[] expected = {0f, 0f, 0f, 0f};
+        Assert.assertArrayEquals(result, expected, 1e-6f);
+    }
+
+    @Test
+    public void testDiv() throws Exception {
+        float[] data = {1f, 2f, 3f, 4f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = new Array(data, dims);
+        Array c = a.div(b);
+
+        float[] result = c.getData();
+        float[] expected = {1f, 1f, 1f, 1f};
+        Assert.assertArrayEquals(result, expected, 1e-6f);
+    }
+
+    @Test
+    public void testMod() throws Exception {
+        float[] dataA = {1f, 2f, 3f, 4f};
+        float[] dataB = {2f, 2f, 2f, 2f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+        Array c = a.mod(b);
+
+        float[] result = c.getData();
+        float[] expected = {1f, 0f, 1f, 0f};
+        Assert.assertArrayEquals(result, expected, 1e-6f);
+    }
+
+    @Test
+    public void testPow() throws Exception {
+        float[] dataA = {1f, 2f, 3f, 4f};
+        float[] dataB = {2f, 2f, 2f, 2f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+        Array c = a.pow(b);
+
+        float[] result = c.getData();
+        float[] expected = {1f, 4f, 9f, 16f};
+        Assert.assertArrayEquals(result, expected, 1e-6f);
+    }
+
+    @Test
+    public void testLt() throws Exception {
+        float[] dataA = {1f, 2f, 3f, 4f};
+        float[] dataB = {2f, 2f, 2f, 2f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+        Array c = a.lt(b);
+
+        boolean[] result = c.getData();
+        boolean[] expected = {true, false, false, false};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testGt() throws Exception {
+        float[] dataA = {1f, 2f, 3f, 4f};
+        float[] dataB = {2f, 2f, 2f, 2f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+        Array c = a.gt(b);
+
+        boolean[] result = c.getData();
+        boolean[] expected = {false, false, true, true};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testLe() throws Exception {
+        float[] dataA = {1f, 2f, 3f, 4f};
+        float[] dataB = {2f, 2f, 2f, 2f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+        Array c = a.le(b);
+
+        boolean[] result = c.getData();
+        boolean[] expected = {true, true, false, false};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testGe() throws Exception {
+        float[] dataA = {1f, 2f, 3f, 4f};
+        float[] dataB = {2f, 2f, 2f, 2f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+        Array c = a.ge(b);
+
+        boolean[] result = c.getData();
+        boolean[] expected = {false, true, true, true};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testEq() throws Exception {
+        float[] dataA = {1f, 2f, 3f, 4f};
+        float[] dataB = {1f, 2f, 3f, 5f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+        Array c = a.eq(b);
+
+        boolean[] result = c.getData();
+        boolean[] expected = {true, true, true, false};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testNe() throws Exception {
+        float[] dataA = {1f, 2f, 3f, 4f};
+        float[] dataB = {1f, 2f, 3f, 5f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+        Array c = a.ne(b);
+
+        boolean[] result = c.getData();
+        boolean[] expected = {false, false, false, true};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testBitAnd() throws Exception {
+        boolean[] dataA = {true, true, true, true};
+        boolean[] dataB = {true, false, true, false};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+        Array c = a.bitAnd(b);
+
+        boolean[] result = c.getData();
+        boolean[] expected = {true, false, true, false};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testBitOr() throws Exception {
+        boolean[] dataA = {true, true, true, true};
+        boolean[] dataB = {true, false, true, false};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+        Array c = a.bitOr(b);
+
+        boolean[] result = c.getData();
+        boolean[] expected = {true, true, true, true};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testBitXor() throws Exception {
+        boolean[] dataA = {true, true, true, true};
+        boolean[] dataB = {true, false, true, false};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(dataA, dims);
+        Array b = new Array(dataB, dims);
+        Array c = a.bitXor(b);
+
+        boolean[] result = c.getData();
+        boolean[] expected = {false, true, false, true};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testBitShiftL() throws Exception {
+        int[] data = {2, 4, 6, 8};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = a.bitShiftL(1);
+
+        int[] result = b.getData();
+        int[] expected = {4, 8, 12, 16};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testBitShiftR() throws Exception {
+        int[] data = {2, 4, 6, 8};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = a.bitShiftR(1);
+
+        int[] result = b.getData();
+        int[] expected = {1, 2, 3, 4};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testTransposeConjugate() throws Exception {
+        DoubleComplex[] data = {new DoubleComplex(0, -1), new DoubleComplex(2, 1),
+                new DoubleComplex(4, 2), new DoubleComplex(0, -2)};
+        long[] dims = {2, 2, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = a.transpose(true);
+
+        DoubleComplex[] result = b.getData();
+        DoubleComplex[] expected = {new DoubleComplex(0, 1), new DoubleComplex(4, -2),
+                new DoubleComplex(2, -1), new DoubleComplex(0, 2)};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testTranspose() throws Exception {
+        float[] data = {1f, 2f, 3f, 4f};
+        long[] dims = {2, 2, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = a.transpose();
+
+        float[] result = b.getData();
+        float[] expected = {1f, 3f, 2f, 4f};
+        Assert.assertArrayEquals(result, expected, 1e-6f);
+    }
+
+    @Test
+    public void testCol() throws Exception {
+        float[] data = {1f, 3f, 2f, 4f};
+        long[] dims = {2, 2, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = a.col(0);
+
+        float[] result = b.getData();
+        float[] expected = {1f, 3f};
+        Assert.assertArrayEquals(result, expected, 1e-6f);
+    }
+
+    @Test
+    public void testCols() throws Exception {
+        float[] data = {1f, 4f, 2f, 5f, 3f, 6f};
+        long[] dims = {2, 3, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = a.cols(0, 1);
+
+        float[] result = b.getData();
+        float[] expected = {1f, 4f, 2f, 5f};
+        Assert.assertArrayEquals(result, expected, 1e-6f);
+    }
+
+    @Test
+    public void testRow() throws Exception {
+        float[] data = {1f, 3f, 2f, 4f};
+        long[] dims = {2, 2, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = a.row(0);
+
+        float[] result = b.getData();
+        float[] expected = {1f, 2f};
+        Assert.assertArrayEquals(result, expected, 1e-6f);
+    }
+
+    @Test
+    public void testRows() throws Exception {
+        float[] data = {1f, 3f, 5f, 2f, 4f, 6f};
+        long[] dims = {3, 2, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = a.rows(0, 1);
+
+        float[] result = b.getData();
+        float[] expected = {1f, 3f, 2f, 4f};
+        Assert.assertArrayEquals(result, expected, 1e-6f);
+    }
+
+    @Test
+    public void testMatmul() throws Exception {
+        float[] data = {1f, 2f, 3f, 4f};
+        long[] dimsA = {4, 1, 1, 1};
+        long[] dimsB = {1, 4, 1, 1};
+        Array a = new Array(data, dimsA);
+        Array b = new Array(data, dimsB);
+        Array c = a.matmul(b);
+
+        float[] result = c.getData();
+        float[] expected = {1f, 2f, 3f, 4f, 2f, 4f, 6f, 8f, 3f, 6f, 9f, 12f, 4f, 8f, 12f, 16f};
+        Assert.assertArrayEquals(result, expected, 1e-6f);
+    }
+
+    @Test
+    public void testCopy() throws Exception {
+        float[] data = {1.1f, 2.1f, 3.1f, 4.1f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = a.copy();
+        Array c = a.eq(b);
+
+        boolean[] result = c.getData();
+        boolean[] expected = {true, true, true, true};
+        Assert.assertArrayEquals(result, expected);
+    }
+
+    @Test
+    public void testAs() throws Exception {
+        float[] data = {1.1f, 2.1f, 3.1f, 4.1f};
+        long[] dims = {4, 1, 1, 1};
+        Array a = new Array(data, dims);
+        Array b = a.as(Array.Dtype.s32);
+
+        int[] result = b.getData();
+        int[] expected = {1, 2, 3, 4};
+        Assert.assertArrayEquals(result, expected);
+    }
 }
