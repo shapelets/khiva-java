f7ac7510b0c4de76fc4a45c47ea813e5dded1df5
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-20 16:34:45 +0200

    Adding support for macOS

--- .CI/travis/install.sh
+++ .CI/travis/install.sh
@@ -6,8 +6,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew install maven
-else
-    sudo apt-get install -y maven
-fi
+# if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+#     brew install maven
+# else
+#     sudo apt-get install -y maven
+# fi
