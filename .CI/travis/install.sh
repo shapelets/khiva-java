e6ed6e8ca0049b17b5bea7db3d8efb5c28b6dd99
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-20 17:42:04 +0200

    Adding support for macOS

--- .CI/travis/install.sh
+++ .CI/travis/install.sh
@@ -9,20 +9,7 @@
 set -euv
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-# # What Java versions do we have?
-# /usr/libexec/java_home -V
 
-# # Prep brew itself
-# brew update
-# brew outdated caskroom/cask/brew-cask || brew upgrade caskroom/cask/brew-cask
-
-# # We must be able to get older Java versions than the latest.
-# brew tap caskroom/versions
-# sudo rm -rf /Library/Java/JavaVirtualMachines
-# brew cask install caskroom/versions/java7
-
-# # Fail unless we installed JDK 7 correctly.
-# /usr/libexec/java_home --failfast --version 1.7
 else
-#    sudo apt-get install -y maven
+
 fi
