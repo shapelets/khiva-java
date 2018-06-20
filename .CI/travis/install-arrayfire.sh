0e9706f1d6c0439eefc105cab59e6644d9ef6d50
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-20 16:16:30 +0200

    Adding support for macOS

--- .CI/travis/install-arrayfire.sh
+++ .CI/travis/install-arrayfire.sh
@@ -5,6 +5,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+mkdir -p installers
+
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     if [ ! -e "installers/ArrayFire-no-gl-v3.5.1_OSX.pkg" ]; then
         wget http://arrayfire.s3.amazonaws.com/3.5.1/ArrayFire-no-gl-v3.5.1_OSX.pkg -O installers/ArrayFire-no-gl-v3.5.1_OSX.pkg
