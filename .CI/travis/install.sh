cb100978fc1aca5277f98e42fed57c3ec59593d1
Antonio Vilches <antonio.vilches@shapelets.io>
2018-06-20 16:43:57 +0200

    Adding support for macOS

--- .CI/travis/install.sh
+++ .CI/travis/install.sh
@@ -6,8 +6,23 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-#     brew install maven
-# else
-#     sudo apt-get install -y maven
-# fi
+set -euv
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+	# What Java versions do we have?
+	/usr/libexec/java_home -V
+
+	# Prep brew itself
+	brew update
+	brew outdated caskroom/cask/brew-cask || brew upgrade caskroom/cask/brew-cask
+
+	# We must be able to get older Java versions than the latest.
+	brew tap caskroom/versions
+	sudo rm -rf /Library/Java/JavaVirtualMachines
+	brew cask install caskroom/versions/java7
+
+	# Fail unless we installed JDK 7 correctly.
+	/usr/libexec/java_home --failfast --version 1.7
+else
+#    sudo apt-get install -y maven
+fi
