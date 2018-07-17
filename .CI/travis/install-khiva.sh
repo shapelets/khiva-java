552f05e7c4239f688136b2baf06fce662211c4f8
Antonio Vilches <antonio.vilches@shapelets.io>
2018-07-17 11:24:55 +0200

    Update install-khiva.sh
    Disabling code coverage on linux

--- .CI/travis/install-khiva.sh
+++ .CI/travis/install-khiva.sh
@@ -73,7 +73,7 @@ else
         make install -j8
     else
         conan install .. --build missing
-        ../../khiva-java/cmakebin/bin/cmake .. -DKHIVA_ENABLE_COVERAGE=ON -DKHIVA_BUILD_DOCUMENTATION=OFF -DKHIVA_BUILD_EXAMPLES=OFF -DKHIVA_BUILD_BENCHMARKS=OFF
+        ../../khiva-java/cmakebin/bin/cmake .. -DKHIVA_BUILD_DOCUMENTATION=OFF -DKHIVA_BUILD_EXAMPLES=OFF -DKHIVA_BUILD_BENCHMARKS=OFF
         sudo make install -j8
         sudo ldconfig
     fi
