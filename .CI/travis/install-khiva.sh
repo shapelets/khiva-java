470e72939ef5284f207518356d934eaa533b362b
Oscar Torreno <oscar.torreno@shapelets.io>
2018-12-27 14:38:43 +0100

    Applying same CI scripts as in python

--- .CI/travis/install-khiva.sh
+++ .CI/travis/install-khiva.sh
@@ -5,7 +5,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-if [["$INSTALL_KHIVA_METHOD" == "installer"]]; then
+if [[ "$INSTALL_KHIVA_METHOD" == "installer" ]]; then
    if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
         if [ ! -e "./installers/khiva-v0.1.0.pkg" ]; then
             wget https://github.com/shapelets/khiva/releases/download/v0.1.0/khiva-v0.1.0-OnlyCPU.pkg -O ./installers/khiva-v0.1.0-OnlyCPU.pkg
@@ -26,7 +26,7 @@ else
     # Install cmake in Linux, it is already installed in osx
     if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
         # Check if the file already exists
-        if [ ! -e "${TRAVIS_BUILD_DIR}/cmake/cmake-3.11.3-Linux-x86_64.sh" ]; then
+        if [ ! -e "${TRAVIS_BUILD_DIR}/cmakebin/cmake-3.13.2-Linux-x86_64.sh" ]; then
             mkdir -p cmakebin
             wget https://github.com/Kitware/CMake/releases/download/v3.13.2/cmake-3.13.2-Linux-x86_64.sh -O cmakebin/cmake-3.13.2-Linux-x86_64.sh
         fi
@@ -35,14 +35,29 @@ else
     fi
 
      #Installing conan and dependencies
+     #Installing conan and dependencies
      if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-        pip install conan
+         sudo apt-get update -y
+         sudo apt-get install -y python3 python3-pip
+         sudo pip3 install setuptools
+         sudo pip3 install conan==1.6.1
      else
-        pip${PYTHON_VERSION} install conan==1.6.1
+         brew upgrade pyenv
+         export TRAVIS_PYTHON_VERSION=3.6.5
+         export PATH=$HOME/.pyenv/shims:$HOME/.pyenv/versions/${TRAVIS_PYTHON_VERSION}/bin:$PATH
+         export PYTHON_VERSION=$(echo $TRAVIS_PYTHON_VERSION | awk -F'.' '{print $1 "." $2}')
+ 
+         pyenv install ${TRAVIS_PYTHON_VERSION} -s
+         pyenv init -
+         pyenv local ${TRAVIS_PYTHON_VERSION}
+
+         # Installing conan
+         sudo pip${PYTHON_VERSION} install --upgrade pip
+         sudo pip${PYTHON_VERSION} install conan
      fi
 
      conan remote add conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
-     if [$? -ne 0]; then
+     if [ $? -ne 0 ]; then
          conan remote update conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
      fi
 
@@ -55,7 +70,7 @@ else
         cmake .. -DKHIVA_ONLY_CPU_BACKEND=ON -DKHIVA_BUILD_DOCUMENTATION=OFF -DKHIVA_BUILD_EXAMPLES=OFF -DKHIVA_BUILD_BENCHMARKS=OFF
          make install -j8
     else
-        conan install .. --build missing
+        conan install .. -s compiler.libcxx=libstdc++11 --build missing
         ../../cmakebin/bin/cmake .. -DKHIVA_ENABLE_COVERAGE=ON -DKHIVA_BUILD_DOCUMENTATION=OFF -DKHIVA_BUILD_EXAMPLES=OFF -DKHIVA_BUILD_BENCHMARKS=OFF
         sudo make install -j8
         sudo ldconfig
