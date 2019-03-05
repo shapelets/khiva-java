c657b0c93a912e5722275a1901d3ee4b3a1a9ba2
Antonio Vilches <antonio.vilches@shapelets.io>
2019-03-05 09:07:11 +0100

    Adding Clustering module.

--- src/main/java/io/shapelets/khiva/Clustering.java
+++ src/main/java/io/shapelets/khiva/Clustering.java
@@ -14,44 +14,39 @@ package io.shapelets.khiva;
  */
 public class Clustering extends Library{
 
-    private native static long[] kMeans(long ref, int bins);
+    private native static long[] kMeans(long tss, int k, float tolerance, int maxIterations);
 
-    private native static long[] kShape(long ref, int bins);
+    private native static long[] kShape(long tss, int k, float tolerance, int maxIterations);
 
     /**
-     * Piecewise Aggregate Approximation (PAA) approximates a time series \(X\) of length \(n\) into vector
-     * \(\bar{X}=(\bar{x}_{1},{@literal ...},\bar{x}_{M})\) of any arbitrary length \(M \leq n\) where each of \(\bar{x_{i}}\) is
-     * calculated as follows:
-     * \[
-     * \bar{x}_{i} = \frac{M}{n} \sum_{j=n/M(i-1)+1}^{(n/M)i} x_{j}.
-     * \]
-     * Which simply means that in order to reduce the dimensionality from \f$n\f$ to \f$M\f$, we first divide the original
-     * time series into \f$M\f$ equally sized frames and secondly compute the mean values for each frame. The sequence
-     * assembled from the mean values is the PAA approximation (i.e., transform) of the original time series.
+     * Calculates the KMeans algorithm.
      *
-     * @param arr  Set of points.
-     * @param bins Sets the total number of divisions.
-     * @return Array of points with the reduced dimensionality.
+     * @param tss               The set of time series to be clusterized.
+     * @param k                 The number of centroids.
+     * @param tolerance         The maximum error tolerance.
+     * @param maxIterations     The maximum number of iterations.
+     * @return  An Array of arrays with the resulting centroids and labels.
      */
-    public static Array paa(Array arr, int bins) {
-        long[] refs = kMeans(arr.getReference(), bins);
-        arr.setReference(refs[0]);
-        return new Array(refs[1]);
+    public static Array[] kMeans(Array tss, int k, float tolerance, int maxIterations) {
+        long[] refs = kMeans(tss.getReference(), k, tolerance, maxIterations);
+        tss.setReference(refs[0]);
+        Array[] result = {new Array(refs[1]), new Array(refs[2])};
+        return result;
     }
 
     /**
-     * Calculates the number of Perceptually Important Points (PIP) in the time series.
-     * <p>
-     * [1] Fu TC, Chung FL, Luk R, and Ng CM. Representing financial time series based on data point importance.
-     * Engineering Applications of Artificial Intelligence, 21(2):277-300, 2008.
+     * Calculates the KShape algorithm.
      *
-     * @param arr       Expects an input array whose dimension zero is the length of the time series.
-     * @param numberIPs The number of points to be returned.
-     * @return The updated ref and an array with the most Perceptually Important numberIPs.
+     * @param tss               The set of time series to be clusterized.
+     * @param k                 The number of centroids.
+     * @param tolerance         The maximum error tolerance.
+     * @param maxIterations     The maximum number of iterations.
+     * @return  An Array of arrays with the resulting centroids and labels.
      */
-    public static Array pip(Array arr, int numberIPs) {
-        long[] refs = kShape(arr.getReference(), numberIPs);
-        arr.setReference(refs[0]);
-        return new Array(refs[1]);
+    public static Array[] kShape(Array tss, int k, float tolerance, int maxIterations) {
+        long[] refs = kShape(tss.getReference(), k, tolerance, maxIterations);
+        tss.setReference(refs[0]);
+        Array[] result = {new Array(refs[1]), new Array(refs[2])};
+        return result;
     }
 }
