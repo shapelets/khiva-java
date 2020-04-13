0dfd90f341af443f420016eec90341c7d0224224
Raúl Bocanegra <raul.bocanegra@shapelets.io>
2020-04-13 19:40:08 +0200

    Force python3 on Travis Remove warnings on Travis yaml

--- .CI/travis/install-khiva.sh
+++ .CI/travis/install-khiva.sh
@@ -31,11 +31,11 @@ if [[ "$INSTALL_KHIVA_METHOD" == "installer" ]]; then
 
 else
     cmake --version
-    python --version
-    pip install --upgrade pip
+    python3 --version
+    pip3 install --upgrade pip
     #Installing conan and dependencies
     if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-        pip install conan==1.22.2
+        pip3 install conan==1.22.2
     else
         pip${PYTHON_VERSION} install conan==1.22.2
     fi
